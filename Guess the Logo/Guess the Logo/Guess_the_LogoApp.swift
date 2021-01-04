7121f64460740ea8240e7a63bce0647f47c9785f
Michał Derej <contact@michalderej.com>
2021-01-04 19:43:46 +0100

    Additional modifier, bug fixes

---
+++ Guess the Logo/Guess the Logo/Guess_the_LogoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_the_LogoApp.swift
+//  Guess the Logo
+//
+//  Created by Michał Derej on 03/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct Guess_the_LogoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Views And Modifiers/Views And Modifiers/Views_And_ModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Views_And_ModifiersApp.swift
+//  Views And Modifiers
+//
+//  Created by Michał Derej on 03/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct Views_And_ModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
