f23f90cc1b28088d286451e1dada49d0b54399f5
Michał Derej <contact@michalderej.com>
2021-02-07 23:50:53 +0100

    Working version of iExpense

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Michał Derej on 06/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
