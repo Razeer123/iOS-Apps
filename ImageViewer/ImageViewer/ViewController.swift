3cefe46a2010e15d0a7e78a7a6a9006e42c7d907
Michał Derej <contact@michalderej.com>
2021-03-07 00:54:49 +0100

    Working version of Image Viewer

---
+++ ImageViewer/ImageViewer/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  ImageViewer
+//
+//  Created by Michał Derej on 04/03/2021.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    var pictures = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Image Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        // Force unwrapping shown below is 100% safe
+        let fileManager = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fileManager.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if (item.hasPrefix("nssl")) {
+                pictures.append(item)
+            }
+        }
+        
+        items.
+        
+        // Debugging purposes
+        print(pictures)
+        
+    }
+    
+    // Tells how many rows do we want
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    // Tells what to do with a particular cell
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = pictures[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+    
+}
