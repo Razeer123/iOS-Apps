c92514f09335e205b4577812ab338226f6cd21c2
Michał Derej <contact@michalderej.com>
2021-03-07 01:30:35 +0100

    Improvements

--- ImageViewer/ImageViewer/ViewController.swift
+++ ImageViewer/ImageViewer/ViewController.swift
@@ -28,7 +28,7 @@ class ViewController: UITableViewController {
             }
         }
         
-        items.
+        pictures.sort()
         
         // Debugging purposes
         print(pictures)
@@ -50,6 +50,8 @@ class ViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
             vc.selectedImage = pictures[indexPath.row]
+            vc.selectedNumber = indexPath.row + 1
+            vc.totalPictures = pictures.count
             navigationController?.pushViewController(vc, animated: true)
         }
     }
